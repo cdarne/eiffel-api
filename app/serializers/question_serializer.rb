046a7aa9051723306b9914fec6a49f4788626612
Cédric Darné <cedric.darne@gmail.com>
2014-06-16 14:27:00 +0200

    Add question dependency in question serializer

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,9 +1,17 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :description, :question_type
+  attributes :id, :description, :dependent_question, :question_type
 
   has_one :select_value
   has_one :rating_value
   has_one :ratio_value
+
+  def include_dependent_question?
+    object.dependent_question_id.present?
+  end
+
+  def dependent_question
+    object.dependent_question.order
+  end
 
   def include_select_value?
     object.question_type == Question::TYPE[:select]
